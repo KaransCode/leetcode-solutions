469c85decbf143b4c5cc5615e61fe5297785b7cb
Karan Singh <karan.ts265@gmail.com>
2026-07-13 23:46:55 +0530

    Time: 68 ms (5.11%), Space: 37.3 MB (5.83%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,4 +1,5 @@
-from collections import Counter
+# from collections import Counter
+from collections import defaultdict
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # frequency = {}
@@ -9,7 +10,15 @@ class Solution:
         #         return True
         # return False
 
-        frequency = Counter(nums)
+        # frequency = Counter(nums)
+        # for key, value in frequency.items():
+        #     if value > 1 :
+        #         return True
+        # return False
+
+        frequency = defaultdict(int)
+        for num in nums:
+            frequency[num] += 1
         for key, value in frequency.items():
             if value > 1 :
                 return True
