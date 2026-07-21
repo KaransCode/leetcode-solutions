001ad973271646a25aba547e5306e52d2858518a
Karan Singh <karan.ts265@gmail.com>
2026-07-21 19:39:20 +0530

    Time: 3 ms (85.08%), Space: 21.2 MB (19.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-
-        # countDict = {}
-        # for i in nums:
-        #     countDict[i] = countDict.get(i,0)+1
-        # majority = max(countDict, key=countDict.get)
-        # return majority if majority >= majority else -1
-
             element = count = 0
             n = len(nums)
             for i in range(n):
