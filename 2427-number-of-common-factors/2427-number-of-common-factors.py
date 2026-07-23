a9f21b5b5d67958922fbc6b0c8dd76dcc7bd03ca
Karan Singh <karan.ts265@gmail.com>
2026-07-24 00:05:08 +0530

    Time: 0 ms (100%), Space: 19.3 MB (17.46%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,12 +1,35 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        limit = min(a,b)
+
+        #1 using GCD
+        def gcd(a,b):
+            while b:
+                a,b = b, a % b
+            return a
+        g = gcd(a,b)
+
         count = 0
-        for i in range(1, limit+1):
-            if a % i == 0 and b % i == 0:
-                count += 1
+        i = 1
+        while i*i <= g:
+            if g % i == 0:
+                if g == i*i:
+                    count += 1
+                else:
+                    count += 2
+            i += 1
         return count
 
+
+        #  Using min. limit for loop 
+        # limit = min(a,b)
+        # count = 0
+        # for i in range(1, limit+1):
+        #     if a % i == 0 and b % i == 0:
+        #         count += 1
+        # return count
+
+        # Using Set and intersection
+        
         # factors_a = set()
         # factors_b = set()
         # factors_a.add(1); factors_a.add(a)
