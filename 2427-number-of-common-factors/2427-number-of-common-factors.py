f676a214afc08c803459781af488b6ec8105fba0
Karan Singh <karan.ts265@gmail.com>
2026-07-23 17:40:29 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.66%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,21 +1,28 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        factors_a = set()
-        factors_b = set()
-        factors_a.add(1); factors_a.add(a)
-        factors_b.add(1); factors_b.add(b)
-        squarerootOfa = a**0.5; squarerootOfa = int(squarerootOfa)
-        squarerootOfb = b**0.5; squarerootOfb = int(squarerootOfb)
-        for i in range(1, squarerootOfa+3):
-            if a % i == 0:
-                factors_a.add(i)
-                if i != squarerootOfa:
-                    factors_a.add(a//i)
+        limit = min(a,b)
+        count = 0
+        for i in range(1, limit+1):
+            if a % i == 0 and b % i == 0:
+                count += 1
+        return count
+
+        # factors_a = set()
+        # factors_b = set()
+        # factors_a.add(1); factors_a.add(a)
+        # factors_b.add(1); factors_b.add(b)
+        # squarerootOfa = a**0.5; squarerootOfa = int(squarerootOfa)
+        # squarerootOfb = b**0.5; squarerootOfb = int(squarerootOfb)
+        # for i in range(1, squarerootOfa+3):
+        #     if a % i == 0:
+        #         factors_a.add(i)
+        #         if i != squarerootOfa:
+        #             factors_a.add(a//i)
                     
-        for i in range(1, squarerootOfb+3):
-            if b % i == 0:
-                factors_b.add(i)
-                if i != squarerootOfb:
-                    factors_b.add(b//i)
-        commonfactors = factors_a.intersection(factors_b)
-        return len(commonfactors)   
+        # for i in range(1, squarerootOfb+3):
+        #     if b % i == 0:
+        #         factors_b.add(i)
+        #         if i != squarerootOfb:
+        #             factors_b.add(b//i)
+        # commonfactors = factors_a.intersection(factors_b)
+        # return len(commonfactors)   
