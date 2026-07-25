764535cfd323946ec673a448ae8607939b3361db
Karan Singh <karan.ts265@gmail.com>
2026-07-25 18:08:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.61%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -12,11 +12,3 @@ class Solution:
             elif num > max2:
                 max2 = num
         return max1 * max2
-
-        # if n <= 0:
-        #     return 0
-        # max_product = 0
-        # result = [int(i) for i in str(n)]
-        # result.sort()
-        # max_product = result[-1] * result[-2]
-        # return max_product
