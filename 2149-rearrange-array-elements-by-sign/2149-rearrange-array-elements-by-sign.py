fc89371f1e14ca2bd204e39da23b43a50e6d572d
Karan Singh <karan.ts265@gmail.com>
2026-07-27 18:23:44 +0530

    Time: 42 ms (71.4%), Space: 42.9 MB (64.02%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -3,12 +3,23 @@ class Solution:
         if not nums:
             return
         n = len(nums)
-        positive = 0; negative = 0; answer = [0] * n
+        positive = 0; negative = 1; answer = [0] * n
         for num in nums:
             if num >= 0:
-                answer[(2*positive)] = num
-                positive += 1
+                answer[positive] = num
+                positive += 2
             else:
-                answer[(2*negative)+1] = num
-                negative += 1
+                answer[negative] = num
+                negative += 2
         return answer
+
+
+        # positive = 0; negative = 0; answer = [0] * n
+        # for num in nums:
+        #     if num >= 0:
+        #         answer[(2*positive)] = num
+        #         positive += 1
+        #     else:
+        #         answer[(2*negative)+1] = num
+        #         negative += 1
+        # return answer
