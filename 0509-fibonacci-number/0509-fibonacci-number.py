1a40a26ab65b97a6be67ffc7dad1400d5bc06a29
Karan Singh <karan.ts265@gmail.com>
2026-07-28 10:55:08 +0530

    Time: 42 ms (82.86%), Space: 19.3 MB (18.71%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = [-1] * (n+1)
+        memo[0] = 0
+        if n >= 1:
+            memo[1] = 1
+
+        def helper(k):
+            if memo[k] != -1:
+                return memo[k]
+            memo[k] = helper(k-1) + helper(k-2)
+            return memo[k]
+        return helper(n)
+
+        # if n == 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        
+        # prev1 = 0
+        # prev2 = 1
+
+        # for _ in range(2,n+1):
+        #     current = prev1 + prev2
+        #     prev1, prev2 = prev2, current
+        # return prev2 
