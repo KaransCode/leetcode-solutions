487b8cd2351d55a21950fc5acce3aab9b25b645b
Karan Singh <karan.ts265@gmail.com>
2026-08-03 00:49:49 +0530

    Time: 31 ms (71.98%), Space: 35 MB (14.15%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -5,7 +5,8 @@ class Solution:
         for num in nums:
             numSet.add(num)
 
-        limit = 2**31
-        for i in range(1,limit):
+        # limit = 2**31
+        limit = abs(max(nums))
+        for i in range(1,limit+2):
             if i not in numSet:
                 return i
