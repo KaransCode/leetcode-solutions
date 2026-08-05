8f3e6a45c6ca9c5efe65e3d9e247f951b5f0d17f
Karan Singh <karan.ts265@gmail.com>
2026-08-05 07:52:53 +0530

    Time: 276 ms (61.61%), Space: 66.5 MB (21.56%) - LeetHub

---
+++ 2878-get-the-size-of-a-dataframe/2878-get-the-size-of-a-dataframe.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    shape = players.shape
+    row = shape[0]
+    col = shape[1]
+    ans = [row,col]
+    return ans
